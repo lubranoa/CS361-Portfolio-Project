54a2a6dee89d0925955e8016866f996fb1b18ad5
Alexander Lubrano <lubranoa@oregonstate.edu>
2022-02-14 22:24:01 -0600

    Add widgets and connect signals, slots, functions
    Add widgets to the password output and password strength sections
    
    Connect button click signals to slots calling functions that will
    eventually send info to microservices that will generate passwords and
    passphrases
    
    Connect spin box and slider signals to slots calling functions that will
    update one's value when the other's is changed
    
    Left side of application involving password/phrase generation selections
    have been modified the most in this commit

--- pw_gen_gui.py
+++ pw_gen_gui.py
@@ -7,11 +7,11 @@
 
 
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QFrame, QLabel,
-                             QHBoxLayout, QVBoxLayout, QCheckBox,
-                             QPushButton, QLineEdit, QSlider, QSpinBox,
-                             QSpacerItem, QSizePolicy)
-from PyQt5.QtCore import Qt
+from PySide6.QtWidgets import (QApplication, QWidget, QFrame, QLabel,
+                               QHBoxLayout, QVBoxLayout, QCheckBox,
+                               QPushButton, QLineEdit, QSlider, QSpinBox,
+                               QSpacerItem, QSizePolicy, QButtonGroup)
+from PySide6.QtCore import Qt
 
 
 class PasswordGenUI(QWidget):
@@ -19,9 +19,8 @@ class PasswordGenUI(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.init_ui()
-
-    def init_ui(self):
+        # self.setGeometry(400, 400, 700, 650)
+        self.setWindowTitle('Password Generator and Tester')
 
         main_layout = QHBoxLayout(self)
 
@@ -35,57 +34,106 @@ class PasswordGenUI(QWidget):
         # Init left layout of main layout
         left_layout = QVBoxLayout(self)
 
-        left_layout.addWidget(QLabel('Generate a Password:'))
+        # Add a new <h3> header for the password section
+        left_layout.addWidget(QLabel('<h3>Generate Password:</h3>', self))
 
-        # Add a horizontal layout to hold a password length slider and spin box
-        # with labels
+        # Create a layout for choosing password length using a synced spin box
+        # and slider with appropriate min and max labels
         length_layout = QHBoxLayout(self)
-        length_layout.addWidget(QLabel('Length'))
-        len_spinbox = QSpinBox(self)
-        length_layout.addWidget(len_spinbox)
-        length_layout.addWidget(QLabel('8'))
-        len_slider = QSlider(self)
-        len_slider.setOrientation(Qt.Horizontal)
-        len_slider.setMinimum(8)
-        len_slider.setMaximum(20)
-        length_layout.addWidget(len_slider)
-        length_layout.addWidget(QLabel('20'))
+        length_layout.addWidget(QLabel('Length', self))
+        self.len_spinbox = QSpinBox(self)
+        self.len_spinbox.setRange(8, 20)
+        length_layout.addWidget(self.len_spinbox)
+        length_layout.addWidget(QLabel('8', self))
+        self.len_slider = QSlider(Qt.Horizontal, self)
+        self.len_slider.setRange(8, 20)
+        length_layout.addWidget(self.len_slider)
+        length_layout.addWidget(QLabel('20', self))
+        # Connect the value changed signals to slots that synchronize the spin
+        # box and slider values
+        self.len_spinbox.valueChanged.connect(self.set_len_slider)
+        self.len_slider.valueChanged.connect(self.set_len_spinbox)
+
+        # Add the password length layout to the left layout
         left_layout.addLayout(length_layout)
 
-        left_layout.addWidget(QCheckBox('Include a - z'))
-        left_layout.addWidget(QCheckBox('Include A - Z'))
-        left_layout.addWidget(QCheckBox('Include 0 - 9'))
-        left_layout.addWidget(QCheckBox('Include !@#$%^&*'))
-        left_layout.addWidget(QLabel('Advanced Options:'))
-        left_layout.addWidget(QCheckBox(
-            'Exclude ambiguous characters l, I, 0, O, etc.'))
-        left_layout.addWidget(QCheckBox(
-            'No duplicate characters'))
-        left_layout.addWidget(QPushButton('Generate Password'))
+        # Create 6 checkboxes and a label and add each to the left layout
+        lowercase_chbx = QCheckBox('Include a - z', self)
+        left_layout.addWidget(lowercase_chbx)
+        uppercase_chbx = QCheckBox('Include A - Z', self)
+        left_layout.addWidget(uppercase_chbx)
+        digit_chbx = QCheckBox('Include 0 - 9', self)
+        left_layout.addWidget(digit_chbx)
+        left_layout.addWidget(QLabel('Advanced Options:', self))
+        special_chbx = QCheckBox('Include !@#$%^&*', self)
+        left_layout.addWidget(special_chbx)
+        ambig_chbx = QCheckBox(
+            'Exclude ambiguous characters i, l, I, L, 0, O, etc.', self)
+        left_layout.addWidget(ambig_chbx)
+        dup_chbx = QCheckBox('No duplicate characters', self)
+        left_layout.addWidget(dup_chbx)
 
-        left_layout.addWidget(QLabel('Generate Passphrase:'))
+        # Create a button group and add all 6 checkboxes to it
+        self.pword_chbxs = QButtonGroup(self)
+        self.pword_chbxs.addButton(lowercase_chbx, 1)
+        self.pword_chbxs.addButton(uppercase_chbx, 2)
+        self.pword_chbxs.addButton(digit_chbx, 3)
+        self.pword_chbxs.addButton(special_chbx, 4)
+        self.pword_chbxs.addButton(ambig_chbx, 5)
+        self.pword_chbxs.addButton(dup_chbx, 6)
 
+        # Set the checkbox group to non-exclusive so that multiple boxes can
+        # be checked at one time
+        self.pword_chbxs.setExclusive(False)
+
+        # Set lowercase and digit boxes to be checked by default
+        lowercase_chbx.setChecked(True)
+        digit_chbx.setChecked(True)
+
+        # Create a "generate password" button, add it to the left layout, and
+        # connect its "clicked" signal to the generate_pword slot
+        pword_gen_btn = QPushButton('Generate Password', self)
+        left_layout.addWidget(pword_gen_btn)
+        pword_gen_btn.clicked.connect(self.generate_pword)
+
+        # Add a new <h3> header for the passphrase section
+        left_layout.addWidget(QLabel('<h3>Generate Passphrase:</h3>', self))
+
+        # Create a layout for choosing passphrase length using a synced spin
+        # box and slider with appropriate min and max labels
         num_words_layout = QHBoxLayout(self)
-        num_words_layout.addWidget(QLabel('Number of Words'))
-        phrase_spinbox = QSpinBox(self)
-        num_words_layout.addWidget(phrase_spinbox)
-        num_words_layout.addWidget(QLabel('2'))
-        phrase_slider = QSlider(self)
-        phrase_slider.setOrientation(Qt.Horizontal)
-        phrase_slider.setMinimum(2)
-        phrase_slider.setMaximum(8)
-        num_words_layout.addWidget(phrase_slider)
-        num_words_layout.addWidget(QLabel('8'))
+        num_words_layout.addWidget(QLabel('Number of Words', self))
+        self.pphrase_spinbox = QSpinBox(self)
+        self.pphrase_spinbox.setRange(2, 8)
+        num_words_layout.addWidget(self.pphrase_spinbox)
+        num_words_layout.addWidget(QLabel('2', self))
+        self.pphrase_slider = QSlider(Qt.Horizontal, self)
+        self.pphrase_slider.setRange(2, 8)
+        num_words_layout.addWidget(self.pphrase_slider)
+        num_words_layout.addWidget(QLabel('8', self))
         left_layout.addLayout(num_words_layout)
+        # Connect the value changed signals to slots that synchronize the spin
+        # box and slider values
+        self.pphrase_spinbox.valueChanged.connect(self.set_pphrase_slider)
+        self.pphrase_slider.valueChanged.connect(self.set_pphrase_spinbox)
 
+        # Create a layout for input of a separator character with a label and
+        # an input line and add it to th left layout
         sep_char_layout = QHBoxLayout(self)
-        sep_char_layout.addWidget(QLabel('Separator Character:'))
-        sep_char_layout.addWidget(QLineEdit(self))
+        sep_char_layout.addWidget(QLabel('Separator Character:', self))
+        self.char_input = QLineEdit(self)
+        sep_char_layout.addWidget(self.char_input)
         left_layout.addLayout(sep_char_layout)
 
-        left_layout.addWidget(QCheckBox('Include a number'))
+        # Create a checkbox that allows the user to include a number
+        self.include_a_num = QCheckBox('Include a number', self)
+        left_layout.addWidget(self.include_a_num)
 
-        left_layout.addWidget(QPushButton('Generate Passphrase'))
+        # Create a "generate passphrase" button, add it to the left layout, and
+        # connect its "clicked" signal to the generate_pphrase slot
+        self.pphrase_gen_btn = QPushButton('Generate Passphrase', self)
+        left_layout.addWidget(self.pphrase_gen_btn)
+        self.pphrase_gen_btn.clicked.connect(self.generate_pphrase)
 
     # -------------------------------------------------------------------------
     # Left Layout of Application
@@ -98,78 +146,130 @@ class PasswordGenUI(QWidget):
         right_layout = QVBoxLayout(self)
 
         # Add widgets to the password output box of the application
-        top_right_box = QVBoxLayout(self)
-        top_right_box.addWidget(QLabel('Password Output:'))
-        top_right_box.addWidget(QLineEdit(self))
-        copy_layout = QHBoxLayout(self)
-        copy_layout.addWidget(QPushButton('Copy'))
-        copy_layout.addWidget(QPushButton('Clear'))
-        top_right_box.addLayout(copy_layout)
+        self.top_right_box = QVBoxLayout(self)
+        self.top_right_spacer = QSpacerItem(10, 20, QSizePolicy.Minimum,
+                                            QSizePolicy.Expanding)
+        self.top_right_box.addItem(self.top_right_spacer)
+        self.top_right_box.addWidget(QLabel('<h3>Password Output:</h3>', self))
+        self.top_right_box.addItem(self.top_right_spacer)
+
+        self.top_right_box.addWidget(QLineEdit(self))
+        self.top_right_box.addItem(self.top_right_spacer)
+
+        self.copy_layout = QHBoxLayout(self)
+        self.copy_layout.addWidget(QPushButton('Copy', self))
+        self.copy_layout.addWidget(QPushButton('Clear', self))
+        self.top_right_box.addLayout(self.copy_layout)
+        self.top_right_box.addItem(self.top_right_spacer)
 
         # Add widgets to the strength tester box of the application
-        bottom_right_box = QVBoxLayout(self)
-        bottom_right_box.addWidget(QLabel('Test Password Strength:'))
-        enter_layout = QHBoxLayout(self)
-        enter_layout.addWidget(QLabel('Enter Password:'))
-        enter_layout.addWidget(QLineEdit())
-        bottom_right_box.addLayout(enter_layout)
-        bottom_right_box.addWidget(QPushButton('Calculate'))
+        self.bottom_right_box = QVBoxLayout(self)
+        self.bott_right_spacer = QSpacerItem(10, 20, QSizePolicy.Minimum,
+                                             QSizePolicy.Expanding)
+        self.bottom_right_box.addItem(self.bott_right_spacer)
 
-        strength_layout = QHBoxLayout(self)
-        strength_layout.addWidget(QLabel('Strength:'))
-        strength_layout.addWidget(QLineEdit(self))
-        bottom_right_box.addLayout(strength_layout)
+        self.bottom_right_box.addWidget(
+            QLabel('<h3>Test Password Strength:</h3>', self))
 
-        t2crack_layout = QHBoxLayout(self)
-        t2crack_layout.addWidget(QLabel('Time to Crack:'))
-        t2crack_layout.addWidget(QLineEdit(self))
-        bottom_right_box.addLayout(t2crack_layout)
+        self.bottom_right_box.addItem(self.bott_right_spacer)
+
+        self.enter_layout = QHBoxLayout(self)
+        self.enter_layout.addWidget(QLabel('Enter Password:', self))
+        self.enter_layout.addWidget(QLineEdit(self))
+        self.bottom_right_box.addLayout(self.enter_layout)
+        self.bottom_right_box.addWidget(QPushButton('Calculate', self))
+
+        self.bottom_right_box.addItem(self.bott_right_spacer)
+
+        self.strength_layout = QHBoxLayout(self)
+        self.strength_layout.addWidget(QLabel('Strength:', self))
+        self.strength_layout.addWidget(QLineEdit(self))
+        self.bottom_right_box.addLayout(self.strength_layout)
+
+        self.t2crack_layout = QHBoxLayout(self)
+        self.t2crack_layout.addWidget(QLabel('Time to Crack:', self))
+        self.t2crack_layout.addWidget(QLineEdit(self))
+        self.bottom_right_box.addLayout(self.t2crack_layout)
+
+        self.bottom_right_box.addItem(self.bott_right_spacer)
 
         # Add the two inner right boxes to the right layout of the main layout
         # and separated by a horizontal line
-        right_layout.addLayout(top_right_box)
-        horiz_split = QFrame(self)
-        horiz_split.setFrameShape(QFrame.HLine)
-        horiz_split.setFrameShadow(QFrame.Sunken)
-        right_layout.addWidget(horiz_split)
-        right_layout.addLayout(bottom_right_box)
+        right_layout.addLayout(self.top_right_box)
+        self.horiz_split = QFrame(self)
+        self.horiz_split.setFrameShape(QFrame.HLine)
+        self.horiz_split.setFrameShadow(QFrame.Sunken)
+        right_layout.addWidget(self.horiz_split)
+        right_layout.addLayout(self.bottom_right_box)
 
         # Add the left and right layouts to the main layout separated by a
         # vertical line and padded by vertical spacers on each side
-        left_spacer1 = QSpacerItem(10, 20, QSizePolicy.Minimum,
-                                   QSizePolicy.Expanding)
-        main_layout.addItem(left_spacer1)
+        self.left_spacer1 = QSpacerItem(10, 20, QSizePolicy.Minimum,
+                                        QSizePolicy.Expanding)
+        main_layout.addItem(self.left_spacer1)
         main_layout.addLayout(left_layout)
-        verti_split = QFrame(self)
-        verti_split.setFrameShape(QFrame.VLine)
-        verti_split.setFrameShadow(QFrame.Sunken)
-        left_spacer2 = QSpacerItem(10, 20, QSizePolicy.Minimum,
-                                   QSizePolicy.Expanding)
-        main_layout.addItem(left_spacer2)
-        main_layout.addWidget(verti_split)
-        right_spacer1 = QSpacerItem(10, 20, QSizePolicy.Minimum,
-                                    QSizePolicy.Expanding)
-        main_layout.addItem(right_spacer1)
+        self.verti_split = QFrame(self)
+        self.verti_split.setFrameShape(QFrame.VLine)
+        self.verti_split.setFrameShadow(QFrame.Sunken)
+        self.left_spacer2 = QSpacerItem(10, 20, QSizePolicy.Minimum,
+                                        QSizePolicy.Expanding)
+        main_layout.addItem(self.left_spacer2)
+        main_layout.addWidget(self.verti_split)
+        self.right_spacer1 = QSpacerItem(10, 20, QSizePolicy.Minimum,
+                                         QSizePolicy.Expanding)
+        main_layout.addItem(self.right_spacer1)
         main_layout.addLayout(right_layout)
-        right_spacer2 = QSpacerItem(10, 20, QSizePolicy.Minimum,
-                                    QSizePolicy.Expanding)
-        main_layout.addItem(right_spacer2)
+        self.right_spacer2 = QSpacerItem(10, 20, QSizePolicy.Minimum,
+                                         QSizePolicy.Expanding)
+        main_layout.addItem(self.right_spacer2)
 
         # Set the window layout to the main layout
-        self.setLayout(main_layout)
+        # self.setLayout(self.main_layout)
 
-        self.setGeometry(400, 400, 600, 550)
-        self.setWindowTitle('Password Generator and Tester')
-        self.show()
+        self.button = QPushButton('click me')
 
+    def set_len_spinbox(self):
+        self.len_spinbox.setValue(self.len_slider.value())
 
-def main():
+    def set_len_slider(self):
+        self.len_slider.setValue(self.len_spinbox.value())
 
-    app = QApplication(sys.argv)
-    pw_gen = PasswordGenUI()
+    def set_pphrase_spinbox(self):
+        self.pphrase_spinbox.setValue(self.pphrase_slider.value())
 
-    sys.exit(app.exec_())
+    def set_pphrase_slider(self):
+        self.pphrase_slider.setValue(self.pphrase_spinbox.value())
+
+    def generate_pword(self):
+
+        pword_params = [0 for x in range(7)]
+
+        for i in range(len(pword_params)):
+            if i == 0:
+                pword_params[i] = self.len_slider.value()
+            else:
+                curr_chbx = self.pword_chbxs.button(i)
+                if curr_chbx.isChecked():
+                    pword_params[i] = 1
+        print('Password parameters:', pword_params)
+
+    def generate_pphrase(self):
+
+        pphrase_params = [0 for x in range(3)]
+
+        pphrase_params[0] = self.pphrase_slider.value()
+        pphrase_params[1] = self.char_input.text()
+        if self.include_a_num.isChecked():
+            pphrase_params[2] = 1
+
+        print('Passphrase Parameters:', pphrase_params)
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+
+    pw_gen = PasswordGenUI()
+    pw_gen.resize(750, 700)
+    pw_gen.show()
+
+    sys.exit(app.exec())
