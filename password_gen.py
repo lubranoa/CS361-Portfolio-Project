b85018e71a37ec78089889710f3ffa51218da03b
Alexander Lubrano <lubranoa@oregonstate.edu>
2022-02-27 09:45:42 -0600

    Reincorporate function structure changes
    Change structure of password_gen.py by putting removal of ambiguous
    chars before adding the character group string to the desired character
    string
    
    Add functionality that, if a character group is selected to be included,
    there must be a mandatory random single character from each selected
    group, then the algorithm fills with random characters from all groups

--- password_gen.py
+++ password_gen.py
@@ -13,60 +13,117 @@ import random
 import string
 
 
-def generate_password(parameters):
+def generate_password(params: dict):
     """
     Generates a password consisting of a specified number of characters, that
     will adhere to one or more of the following:
         - includes lowercase letters
-        - includes digits
         - includes uppercase letters
+        - includes digits
         - includes a short list of special characters
         - excludes similar characters that could be mistaken for each other
         - excludes duplicate characters
+        - include a minimum number of digits
+        - include a minimum number of special characters
 
-    Takes a Python dictionary with data from the GUI that tells the function
-    what customization to incorporate into the generated password
+    Dictionary must contain a password length, a lowercase Bool, an uppercase
+    Bool, a digits Bool, a special character Bool, exclude ambiguous Bool,
+    exclude duplicates Bool, a minimum number of digits, and a minimum number
+    of special characters using these strings as keys:
+
+    'len', 'low_case', 'upp_case', 'digits', 'special', 'no_ambig', 'no_dup',
+    'min_dig', 'min_spec'
 
     Returns a generated password that adheres to the specified criteria
     """
 
-    # TODO: Change function to handle Python dictionaries instead of arrays
     # TODO: Add functionality for minimum number of digits and minimum number
     #       of special characters
 
-    # 'len', 'low_case', 'upp_case', 'digits', 'special',
-    # 'no_ambig', 'no_dup', 'min_dig', 'min_spec'
     spec_chars = '!@#$%^&*'
-    remove_chars = 'Il1O0'
-
-    desired_len = parameters['len']
-
+    ambig_chars = 'Il1O0'
     desired_chars = ''
     password_chars = []
-    password = ''
 
-    if parameters['low_case']:
-        desired_chars = desired_chars + string.ascii_lowercase
-    if parameters['upp_case']:
-        desired_chars = desired_chars + string.ascii_uppercase
-    if parameters['digits']:
-        desired_chars = desired_chars + string.digits
-    if parameters['special']:
+    # If "include a-z" selected, picks single random a-z char, add it to
+    # password chars list, and concats lower str to desired str
+    if params['low_case']:
+        lower_chars = string.ascii_lowercase
+
+        # If "no ambiguous chars" selected, removes 'l' from lower str
+        if params['no_ambig']:
+            lower_chars.replace('l', '')
+
+        mand_char = random.choice(lower_chars)
+
+        # If "no duplicates" selected, removes chosen char from desired str
+        if params['no_dup']:
+            lower_chars.replace(mand_char, '')
+
+        password_chars.append(mand_char)
+        desired_chars = desired_chars + lower_chars
+
+    # If "include A-Z" selected, picks single random A-Z char, add it to
+    # password chars list, and concats upper str to desired str
+    if params['upp_case']:
+        upper_chars = string.ascii_uppercase
+
+        # If user selected "no ambiguous chars", ensures an "I" or an "O" are
+        # not chosen for a mandatory uppercase letter
+        if params['no_ambig']:
+            upper_chars.replace('I', '')
+            upper_chars.replace('O', '')
+
+        mand_char = random.choice(string.ascii_uppercase)
+
+        if params['no_dup']:
+            upper_chars.replace(mand_char, '')
+
+        password_chars.append(mand_char)
+        desired_chars = desired_chars + upper_chars
+
+    # If "include 0-9" selected, picks single random 0-9 char, add it to
+    # password chars list, and concats digit str to desired str
+    if params['digits']:
+        digit_chars = string.digits
+
+        # If "no ambiguous chars" selected, removes "0" and "1" from digit str
+        if params['no_ambig']:
+            digit_chars.replace('0', '')
+            digit_chars.replace('1', '')
+
+        mand_char = random.choice(digit_chars)
+
+        # If "no duplicates" selected, removes chosen digit from digit str
+        if params['no_dup']:
+            digit_chars.replace(mand_char, '')
+
+        password_chars.append(mand_char)
+        desired_chars = desired_chars + digit_chars
+
+    if params['special']:
+        mand_char = random.choice(spec_chars)
+
+        if params['no_dup']:
+            spec_chars.replace(mand_char, '')
+
+        password_chars.append(mand_char)
         desired_chars = desired_chars + spec_chars
-    if parameters['no_ambig']:
-        for char in remove_chars:
+
+    if params['no_ambig']:
+        for char in ambig_chars:
             desired_chars = desired_chars.replace(char, '')
 
     print(desired_chars)
     desired_chars = random.sample(desired_chars, k=len(desired_chars))
 
-    if parameters['no_dup']:
-        while len(password_chars) < desired_len and len(desired_chars) > 0:
+    if params['no_dup']:
+        while len(password_chars) < params['len'] and len(desired_chars) > 0:
             new_char = random.choice(desired_chars)
             password_chars.append(new_char)
             desired_chars.remove(new_char)
     else:
-        while len(password_chars) < desired_len and len(desired_chars) > 0:
+        while len(password_chars) < params['len'] and len(desired_chars) > 0:
             new_char = random.choice(desired_chars)
             password_chars.append(new_char)
 
@@ -79,6 +136,16 @@ def generate_password(parameters):
 
 
 if __name__ == '__main__':
-    pword_params = [20, 1, 1, 1, 1, 1, 1]
+    pword_params = {
+        'len': 20,
+        'low_case': True,
+        'upp_case': True,
+        'digits': True,
+        'special': True,
+        'no_ambig': True,
+        'no_dup': True,
+        'min_dig': 1,
+        'min_spec': 1
+    }
     pword = generate_password(pword_params)
     print(pword)
