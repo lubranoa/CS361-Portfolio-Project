f74b48372e289e05887872127046ab7cf0a219ee
Alexander Lubrano <lubranoa@oregonstate.edu>
2022-02-27 10:16:06 -0600

    Update documentation

--- password_gen.py
+++ password_gen.py
@@ -41,7 +41,6 @@ def generate_password(params: dict):
     #       of special characters
 
     spec_chars = '!@#$%^&*'
-    ambig_chars = 'Il1O0'
     desired_chars = ''
     password_chars = []
 
@@ -60,78 +59,85 @@ def generate_password(params: dict):
         if params['no_dup']:
             lower_chars.replace(mand_char, '')
 
+        # Append chosen char to password char list
         password_chars.append(mand_char)
+
+        # Concat lower str with desired str
         desired_chars = desired_chars + lower_chars
 
-    # If "include A-Z" selected, picks single random A-Z char, add it to
-    # password chars list, and concats upper str to desired str
+    # If "include A-Z" selected, picks single random A-Z char, excluding "I"
+    # and "O" if "no ambig chars" selected, removing chosen char from desired
+    # str if "no dups" selected, add it to password chars list, and concats
+    # upper str to desired str
     if params['upp_case']:
         upper_chars = string.ascii_uppercase
-
-        # If user selected "no ambiguous chars", ensures an "I" or an "O" are
-        # not chosen for a mandatory uppercase letter
         if params['no_ambig']:
             upper_chars.replace('I', '')
             upper_chars.replace('O', '')
 
         mand_char = random.choice(string.ascii_uppercase)
-
         if params['no_dup']:
             upper_chars.replace(mand_char, '')
 
         password_chars.append(mand_char)
         desired_chars = desired_chars + upper_chars
 
-    # If "include 0-9" selected, picks single random 0-9 char, add it to
-    # password chars list, and concats digit str to desired str
+    # If "include 0-9" selected, picks single random 0-9 char, excluding "0"
+    # and "1" if "no ambig chars" selected, removing chosen char from desired
+    # str if "no dups" selected, add it to password chars list, and concats
+    # digit str to desired str
     if params['digits']:
         digit_chars = string.digits
-
-        # If "no ambiguous chars" selected, removes "0" and "1" from digit str
         if params['no_ambig']:
             digit_chars.replace('0', '')
             digit_chars.replace('1', '')
 
         mand_char = random.choice(digit_chars)
-
-        # If "no duplicates" selected, removes chosen digit from digit str
         if params['no_dup']:
             digit_chars.replace(mand_char, '')
 
         password_chars.append(mand_char)
         desired_chars = desired_chars + digit_chars
 
+    # If "include special" selected, picks single random special char, removing
+    # chosen char from desired str if "no dups" selected, add it to password
+    # chars list, and concats special str to desired str
     if params['special']:
         mand_char = random.choice(spec_chars)
-
         if params['no_dup']:
             spec_chars.replace(mand_char, '')
 
         password_chars.append(mand_char)
         desired_chars = desired_chars + spec_chars
 
+    # Ensure no other ambiguous characters are in desired str
     if params['no_ambig']:
-        for char in ambig_chars:
+        for char in 'Il1O0':
             desired_chars = desired_chars.replace(char, '')
 
-    print(desired_chars)
+    # Splits desired string into randomized list of desired chars
     desired_chars = random.sample(desired_chars, k=len(desired_chars))
 
+    # If "no dups" selected, removes chosen chars from desired list as they are
+    # chosen and added to password char list
     if params['no_dup']:
+        # Runs until password list is correct length or desired chars runs out
+        # of chars to choose from (certain password configurations)
         while len(password_chars) < params['len'] and len(desired_chars) > 0:
             new_char = random.choice(desired_chars)
             password_chars.append(new_char)
             desired_chars.remove(new_char)
+    # Otherwise, chars chosen randomly from desired list to fill password char
+    # list to desired password size
     else:
-        while len(password_chars) < params['len'] and len(desired_chars) > 0:
+        while len(password_chars) < params['len']:
             new_char = random.choice(desired_chars)
             password_chars.append(new_char)
 
+    # Shuffle password char list, then create and return password
     random.shuffle(password_chars)
     password = ''.join(password_chars)
-
     print('password length:', len(password))
-
     return password
 
 
