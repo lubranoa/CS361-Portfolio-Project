2524c316d361c832af3bf9b7e06104855cc1a772
Alexander Lubrano <lubranoa@oregonstate.edu>
2022-02-27 09:25:55 -0600

    Revert "Change params and remove QButtonGroup"
    This reverts commit 63bde662b089e2a07111011c582df5082813088b.
    
    Revert to keep parameters passed as a Python dictionary. Need to have
    QButtonGroup for generation of parameter dictionary.

--- password_gen.py
+++ password_gen.py
@@ -13,9 +13,7 @@ import random
 import string
 
 
-def generate_password(length: int, lower: bool, nums: bool, upper: bool,
-                      special: bool, no_ambig: bool, no_dups: bool,
-                      min_num: int, min_spec: int):
+def generate_password(parameters):
     """
     Generates a password consisting of a specified number of characters, that
     will adhere to one or more of the following:
@@ -25,104 +23,50 @@ def generate_password(length: int, lower: bool, nums: bool, upper: bool,
         - includes a short list of special characters
         - excludes similar characters that could be mistaken for each other
         - excludes duplicate characters
-        - include a minimum number of digits
-        - include a minimum number of special characters
 
-    Takes a password length, a lowercase Bool, a numbers Bool, an uppercase
-    Bool, a special character Boolean, exclude ambiguous Bool, exclude
-    duplicates Bool, a minimum number of digits, and a minimum number of
-    special characters.
+    Takes a Python dictionary with data from the GUI that tells the function
+    what customization to incorporate into the generated password
 
     Returns a generated password that adheres to the specified criteria
     """
 
+    # TODO: Change function to handle Python dictionaries instead of arrays
     # TODO: Add functionality for minimum number of digits and minimum number
     #       of special characters
 
+    # 'len', 'low_case', 'upp_case', 'digits', 'special',
+    # 'no_ambig', 'no_dup', 'min_dig', 'min_spec'
     spec_chars = '!@#$%^&*'
-    ambig_chars = 'Il1O0'
+    remove_chars = 'Il1O0'
+
+    desired_len = parameters['len']
+
     desired_chars = ''
     password_chars = []
+    password = ''
 
-    # If "include a-z" selected, picks single random a-z char, add it to
-    # password chars list, and concats lower str to desired str
-    if lower:
-        lower_chars = string.ascii_lowercase
-
-        # If "no ambiguous chars" selected, removes 'l' from lower str
-        if no_ambig:
-            lower_chars.replace('l', '')
-
-        mand_char = random.choice(lower_chars)
-
-        # If "no duplicates" selected, removes chosen char from desired str
-        if no_dups:
-            lower_chars.replace(mand_char, '')
-
-        password_chars.append(mand_char)
-        desired_chars = desired_chars + lower_chars
-
-    # If "include 0-9" selected, picks single random 0-9 char, add it to
-    # password chars list, and concats digit str to desired str
-    if nums:
-        digit_chars = string.digits
-
-        # If "no ambiguous chars" selected, removes "0" and "1" from digit str
-        if no_ambig:
-            digit_chars.replace('0', '')
-            digit_chars.replace('1', '')
-
-        mand_char = random.choice(digit_chars)
-
-        # If "no duplicates" selected, removes chosen digit from digit str
-        if no_dups:
-            digit_chars.replace(mand_char, '')
-
-        password_chars.append(mand_char)
-        desired_chars = desired_chars + digit_chars
-
-    # If "include A-Z" selected, picks single random A-Z char, add it to
-    # password chars list, and concats upper str to desired str
-    if upper:
-        upper_chars = string.ascii_uppercase
-
-        # If user selected "no ambiguous chars", ensures an "I" or an "O" are
-        # not chosen for a mandatory uppercase letter
-        if no_ambig:
-            upper_chars.replace('I', '')
-            upper_chars.replace('O', '')
-
-        mand_char = random.choice(string.ascii_uppercase)
-
-        if no_dups:
-            upper_chars.replace(mand_char, '')
-
-        password_chars.append(mand_char)
-        desired_chars = desired_chars + upper_chars
-
-    if special:
-        mand_char = random.choice(spec_chars)
-
-        if no_dups:
-            spec_chars.replace(mand_char, '')
-
-        password_chars.append(mand_char)
+    if parameters['low_case']:
+        desired_chars = desired_chars + string.ascii_lowercase
+    if parameters['upp_case']:
+        desired_chars = desired_chars + string.ascii_uppercase
+    if parameters['digits']:
+        desired_chars = desired_chars + string.digits
+    if parameters['special']:
         desired_chars = desired_chars + spec_chars
-
-    if no_ambig:
-        for char in ambig_chars:
+    if parameters['no_ambig']:
+        for char in remove_chars:
             desired_chars = desired_chars.replace(char, '')
 
     print(desired_chars)
     desired_chars = random.sample(desired_chars, k=len(desired_chars))
 
-    if no_dups:
-        while len(password_chars) < length and len(desired_chars) > 0:
+    if parameters['no_dup']:
+        while len(password_chars) < desired_len and len(desired_chars) > 0:
             new_char = random.choice(desired_chars)
             password_chars.append(new_char)
             desired_chars.remove(new_char)
     else:
-        while len(password_chars) < length and len(desired_chars) > 0:
+        while len(password_chars) < desired_len and len(desired_chars) > 0:
             new_char = random.choice(desired_chars)
             password_chars.append(new_char)
 
