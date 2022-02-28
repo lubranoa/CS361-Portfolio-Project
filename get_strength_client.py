cc36f2b686c7f7b52af40cee8a37ba80b2b2cb04
Alexander Lubrano <lubranoa@oregonstate.edu>
2022-02-28 15:34:39 -0600

    Modify structure, fix connection error
    Move descriptive text dictionary out of function
    
    Fix connection error by moving client object init and client connection
    call inside of get_strength function
    
    Make unneeded close_strength_client function non-functional

--- get_strength_client.py
+++ get_strength_client.py
@@ -8,34 +8,34 @@
 
 import socket
 
+stren_score_strs = {'0': '0 - Too Guessable: Very Risky Password',
+                    '1': '1 - Very Guessable: Risky Password',
+                    '2': '2 - Somewhat Guessable: Okay Password',
+                    '3': '3 - Safely Unguessable: Good Password',
+                    '4': '4 - Very Unguessable: Great Password'}
 MSG_SIZE = 1024
 PORT = 12345
 HOST = socket.gethostbyname(socket.gethostname())
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST, PORT))
-
 
 def get_strength(password):
     """Does this"""
-
-    stren_score_strs = {'0': '0 - Too Guessable: Very Risky Password',
-                        '1': '1 - Very Guessable: Risky Password',
-                        '2': '2 - Somewhat Guessable: Okay Password',
-                        '3': '3 - Safely Unguessable: Good Password',
-                        '4': '4 - Very Unguessable: Great Password'}
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((HOST, PORT))
 
     client.send(password.encode())
 
     strength_score = client.recv(MSG_SIZE).decode()
     time_to_crack = client.recv(MSG_SIZE).decode()
 
+    client.close()
+
     return [stren_score_strs[strength_score], time_to_crack]
 
 
 def close_strength_client():
     """Does thsis"""
-    client.close()
+    pass
 
 
 if __name__ == '__main__':
